83b54d128f5400ca81dc9ee7bc649b985b0631f0
ranagithubrr <ranarr.dev@gmail.com>
2024-07-06 21:49:13 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+      './src/**/*.{html,js,ts,jsx,tsx}', // Adjust the path to your source files
+    ],
+    theme: {
+      extend: {},
+    },
+    screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1000px",
+        xl: "1280px",
+        "2xl": "1636px",
+      },
+    plugins: [],
+  }
